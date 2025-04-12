af5db60078c8c8faf821362d60a264c374babe47
anshu2k24 <anshu799pati@gmail.com>
2025-04-12 07:12:24 +0530

    Basic pass gen implemented!

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import pandas as pd
+import string as str
+import random as r
 
 if __name__ == '__main__':
     df = pd.read_csv('u_p.csv')
 
+    # punctuation="#$%!@*&"
+    pass_elements = str.ascii_letters+str.punctuation+str.digits
+    n = int(input("Length: "))
+    pass_generated = ''.join(r.choices(pass_elements,k=n))
+    # print(pass_generated)
+
     username = input("UserName: ")
-    password = input("Password: ")
+    password = pass_generated
+    # password = input("Password: ")
 
     new = pd.DataFrame([{"UserName":username,"Password":password}])
     df = pd.concat([df,new],ignore_index=True)
