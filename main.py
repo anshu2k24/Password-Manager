d8c4ec5ce045abb833fd08e83cb27bb4f5bd4ff1
anshu2k24 <anshu799pati@gmail.com>
2025-04-12 16:41:01 +0530

    v2 - able to store username, gen a password if reqd and more user friendly now

--- main.py
+++ main.py
@@ -5,18 +5,25 @@ import random as r
 if __name__ == '__main__':
     df = pd.read_csv('u_p.csv')
 
-    # punctuation="#$%!@*&"
-    pass_elements = str.ascii_letters+str.punctuation+str.digits
-    n = int(input("Length: "))
-    pass_generated = ''.join(r.choices(pass_elements,k=n))
-    # print(pass_generated)
+    punctuation="#$%!@*&"
+    pass_elements = str.ascii_letters+ punctuation +str.digits
 
+    #username
     username = input("UserName: ")
-    password = pass_generated
-    # password = input("Password: ")
 
+    #password
+    y_n = input("Do you want to generate a password? Y/N: ")
+    if y_n=='Y' or 'y':
+        n = int(input("Length: "))
+        pass_generated = ''.join(r.choices(pass_elements,k=n))
+        password = pass_generated
+    else:
+        password = input("Password: ")
+
+    #storing username and password
     new = pd.DataFrame([{"UserName":username,"Password":password}])
     df = pd.concat([df,new],ignore_index=True)
     df.to_csv('u_p.csv',index=False)
+    print("TADA!! Done")
 
     
