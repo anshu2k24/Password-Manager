4a66099973fe4a0e68bfe1b89006e19c0d701aed
anshu2k24 <anshu799pati@gmail.com>
2025-04-17 08:12:30 +0530

    store_function made, pass/salt stored in plain txt,csv file handle improved, encrypt_func working, lot of work done 👌

--- Backend/main.py
+++ Backend/main.py
@@ -55,53 +55,61 @@ if __name__ == '__main__':
         return password
     
     #encryption_decryption
-    # def encrypt_function(password,m_password):
-    #     salt = os.urandom(16)
-    #     kdf = PBKDF2HMAC(
-    #         algorithm=hashes.SHA256(),
-    #         length=32,
-    #         salt=salt,
-    #         iterations=1_200_000,
-    #     )
-    #     key = base64.urlsafe_b64encode(kdf.derive(m_password.encode()))
-    #     f_obj = F(key)
-    #     encrypted_pass = f_obj.encrypt(password.encode())
-    #     password = encrypted_pass
-    #     return salt
+    def encrypt_function(password,m_password):
+        salt = os.urandom(16)
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=1_200_000,
+        )
+        key = base64.urlsafe_b64encode(kdf.derive(m_password.encode()))
+        f_obj = F(key)
+        encrypted_pass = f_obj.encrypt(password.encode())
+        #b64encode and decode this below converts raw bytes into plain text which makes it portable,easy to store and read from csv
+        encrypted_pass_b64 = base64.b64encode(encrypted_pass).decode()  
+        salt_b64 = base64.b64encode(salt).decode()
+        return encrypted_pass_b64,salt_b64
+    
+    #storing username and password
+    def store_function(platform,username,password,salt):
+
+        #reading csv
+        try:
+            df = pd.read_csv('u_p.csv')
+        
+        except FileNotFoundError:
+            df = pd.DataFrame(columns=["Platform","Username","Password","Salt"])
+
+        new = pd.DataFrame(
+            [
+                {
+                    "Platform":platform,
+                    "Username":username,
+                    "Password":password,
+                    "Salt":salt
+                }
+            ]
+        )
+        df = pd.concat([df,new],ignore_index=True)
+        df.to_csv('u_p.csv',index=False)
+        print("TADA!! Done")
                           
 
     #calling for all functions
     platform = platform_function()
     username = username_function()
     password = password_function()
+    password,salt = encrypt_function(password,m_password)
+    store_function(platform,username,password,salt)
 
-    salt = os.urandom(16)
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        length=32,
-        salt=salt,
-        iterations=1_200_000,
-    )
-    key = base64.urlsafe_b64encode(kdf.derive(m_password.encode()))
-    f_obj = F(key)
-    encrypted_pass = f_obj.encrypt(password.encode())
-    password = encrypted_pass
-
-
-    # salt = encrypt_function(password,m_password)
-    # encrypt_function(password,m_password)
+    
      
 
-    #reading csv
-    try:
-        df = pd.read_csv('u_p.csv')
-    except FileNotFoundError:
-        df = pd.DataFrame(columns=["Platform","Username","Password","Salt"])
 
-    #storing username and password
-    new = pd.DataFrame([{"Platform":platform,"Username":username,"Password":password,"Salt":salt}])
-    df = pd.concat([df,new],ignore_index=True)
-    df.to_csv('u_p.csv',index=False)
-    print("TADA!! Done")
+
+
+
+    
 
  
